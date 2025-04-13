f28e0ef254f81a302aa775b3d052b328cd4b5caf
Em Ruszanowski <em@rusz.dev>
2025-04-12 21:33:19 -0400

    Add async Postgres connection pooling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
+use diesel::prelude::*;
+use diesel_async::pooled_connection::AsyncDieselConnectionManager;
+use diesel_async::pooled_connection::deadpool::{Object, Pool};
+use diesel_async::{AsyncPgConnection, RunQueryDsl};
 use dotenvy::dotenv;
 use poise::serenity_prelude as serenity;
+use std::sync::Arc;
 
 pub mod commands;
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-pub struct Data {}
+pub struct Data {
+    database: Pool<AsyncPgConnection>,
+}
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     let token = std::env::var("DISCORD_TOKEN").expect("Missing DISCORD_TOKEN.");
     let intents = serenity::GatewayIntents::non_privileged();
+
+    let config = AsyncDieselConnectionManager::<AsyncPgConnection>::new(
+        std::env::var("DATABASE_URL").expect("Missing DATABASE_URL."),
+    );
+    let pool = Pool::builder(config)
+        .build()
+        .expect("Failed to build database pool.");
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
@@ -22,7 +36,7 @@ async fn main() {
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
+                Ok(Data { database: pool })
             })
         })
         .build();
