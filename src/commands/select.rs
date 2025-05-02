4ec5994f58803142039aefd1d64d7aafab60884c
Em Ruszanowski <em@rusz.dev>
2025-05-01 23:19:44 -0400

    Add member select

--- src/commands/select.rs
+++ src/commands/select.rs
@@ -1,13 +1,17 @@
 use crate::models::{Guild, Member};
+use crate::schema::member::birthday;
 use crate::schema::{guild, member};
 use crate::{Context, Error};
+use chrono::NaiveDateTime;
+use diesel::prelude::*;
 use diesel_async::RunQueryDsl;
+use poise::serenity_prelude as serenity;
 
 #[poise::command(
     slash_command,
     owners_only,
     hide_in_help,
-    subcommands("all_members", "all_guilds")
+    subcommands("all_members", "all_guilds", "member")
 )]
 pub async fn select(_ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
@@ -30,5 +34,21 @@ pub async fn all_guilds(ctx: Context<'_>) -> Result<(), Error> {
     let data: Vec<Guild> = guild::table.load(&mut conn).await?;
 
     ctx.say(format!("{:?}", data)).await?;
+    Ok(())
+}
+
+#[poise::command(slash_command)]
+pub async fn member(
+    ctx: Context<'_>,
+    #[description = "Member"] member: serenity::Member,
+) -> Result<(), Error> {
+    let mut connection = ctx.data().connection_pool.get().await?;
+    let member_birthday: NaiveDateTime = member::table
+        .select(birthday)
+        .filter(member::snowflake.eq(member.user.id.to_string()))
+        .first(&mut connection)
+        .await?;
+
+    ctx.say(format!("{:?}", member_birthday)).await?;
     Ok(())
 }
